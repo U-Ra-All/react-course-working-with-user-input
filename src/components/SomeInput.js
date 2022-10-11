f940d3f207560811785bb6f826fabde2c82d0a48
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-10-11 12:21:59 +0300

    04-Отправка Формы и Получение Данных Пользователя

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,9 +1,31 @@
+import { useState, useRef } from "react";
+
 const SomeInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+    setEnteredName("");
+    // nameInputRef.current.value = ""; not best practice
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Отправить</button>
