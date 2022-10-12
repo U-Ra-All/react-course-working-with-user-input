b00875b77d5ec1d3def1110820a7ec3926c658e3
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-10-12 12:02:09 +0300

    06-Реагирование на Потерю Фокуса1

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,13 +1,32 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const SomeInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [isEnteredNameValid, setIsEnteredNameValid] = useState(false);
+  const [wasNameInputTouched, setWasNameInputTouched] = useState(false);
+
+  useEffect(() => {
+    if (isEnteredNameValid) {
+      console.log("Данные в инпуте валидны");
+    }
+  }, [isEnteredNameValid]);
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    setWasNameInputTouched(true);
+
+    if (enteredName.trim() === "") {
+      setIsEnteredNameValid(false);
+      return;
+    }
+
+    setIsEnteredNameValid(true);
 
     console.log(enteredName);
     console.log(nameInputRef.current.value);
@@ -15,9 +34,15 @@ const SomeInput = (props) => {
     // nameInputRef.current.value = ""; not best practice
   };
 
+  const isNameInputInvalid = !isEnteredNameValid && wasNameInputTouched;
+
+  const nameInputClasses = isNameInputInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Введите Имя</label>
         <input
           ref={nameInputRef}
@@ -26,6 +51,9 @@ const SomeInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {isNameInputInvalid && (
+          <p className="error-text">Нужно обязательно ввести имя</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Отправить</button>
